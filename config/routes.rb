fb80b55df9e7e0ff7b7e8d52a75b1adf42faf5cb
yusa0730 <isizawa5226@gmail.com>
2021-01-24 22:32:29 +0900

    routeの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        # only: %i[index]のかたちで特定のルーティングしか生成しないという方法もあります。
+        resources :foods, only: %i[index]
+      end
+      resources :foods, only: %i[index create]
+      # 「'line_foods/replace'というURLに対してPUTリクエストがきたら、line_foods_controller.rbのreplaceメソッドを呼ぶ」ということを意味します。
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
